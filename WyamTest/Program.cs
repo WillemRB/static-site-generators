9af6c77c619838144387e6dfd28297cb603d171c
WillemRB <wrb@outlook.com>
2016-01-30 11:29:44 +0100

    Add test application

---
+++ WyamTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WyamTest
+{
+    /// <summary>
+    /// Summary of the Program
+    /// </summary>
+    /// <remarks>
+    /// Remarks about Program
+    /// </remarks>
+    class Program
+    {
+        /// <summary>
+        /// The summary of Main
+        /// </summary>
+        /// <param name="args">Arguments</param>
+        static void Main(string[] args)
+        {
+        }
+    }
+}
