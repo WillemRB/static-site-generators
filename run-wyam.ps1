1c93bbde10e739a943fb856f743c206f161e81b6
Willem Bouma <wrb@outlook.com>
2016-04-04 11:27:57 +0200

    Setting up Json to Html

--- run-wyam.ps1
+++ run-wyam.ps1
@@ -1,9 +1,9 @@
 Clear-Host
 
-$wyam = ''
+$wyam = 'WyamBin\Wyam.exe'
 
-$config = Join-Path $PSScriptRoot 'Wyam/config.wyam'
-$input = Join-Path $PSScriptRoot 'Wyam/Input'
-$output = Join-Path $PSScriptRoot 'Wyam/Output'
+$config = Join-Path $PSScriptRoot 'JsonHtml/config.wyam'
+$input = Join-Path $PSScriptRoot 'JsonHtml/Input'
+$output = Join-Path $PSScriptRoot 'JsonHtml/Output'
 
 . $wyam --config $config --input $input --output $output
